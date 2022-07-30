1cf8a5ebf424676b8eaa9b894a76fc21c53a9bd3
MateussDev <thisismateussdev@gmail.com>
2022-07-30 23:13:09 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListElementComponent } from './list-element/list-element.component';
+import { LevelListComponent } from './level-list/level-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListElementComponent,
+    LevelListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/list-element/list-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-element',
+  templateUrl: './list-element.component.html',
+  styleUrls: ['./list-element.component.css']
+})
+export class ListElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
