67af47c5cd8d0c464bc818bd49a3a24c34e775b2
MateussDev-backrooms <thisismateussdev@gmail.com>
2022-08-14 20:19:30 +0300

    Finalization of v2 att 2

---
+++ src/app/creator-leaderboard/creator-leaderboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creator-leaderboard',
+  templateUrl: './creator-leaderboard.component.html',
+  styleUrls: ['./creator-leaderboard.component.css']
+})
+export class CreatorLeaderboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
